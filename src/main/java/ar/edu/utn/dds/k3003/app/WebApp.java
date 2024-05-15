ee3d853689f555698b4fef55dd9cd4c24c67ad85
Ezequiel <ezequieljsosa@gmail.com>
2024-05-15 20:43:34 -0300

    Agregado de config de jackson en Javalin

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.javalin.Javalin;
+import io.javalin.json.JavalinJackson;
+
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -24,7 +26,11 @@ public class WebApp {
 
     var port = Integer.parseInt(env.getOrDefault("PORT", "8080"));
 
-    var app = Javalin.create().start(port);
+    var app = Javalin.create(config -> {
+      config.jsonMapper(new JavalinJackson().updateMapper(mapper -> {
+        configureObjectMapper(mapper);
+      }));
+    }).start(port);
 
     var rutaController = new RutaController(fachada);
     var trasladosController = new TrasladoController(fachada);
@@ -36,12 +42,16 @@ public class WebApp {
 
   public static ObjectMapper createObjectMapper() {
     var objectMapper = new ObjectMapper();
+    configureObjectMapper(objectMapper);
+    return objectMapper;
+  }
+
+  public static void configureObjectMapper(ObjectMapper objectMapper) {
     objectMapper.registerModule(new JavaTimeModule());
     objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     var sdf = new SimpleDateFormat(Constants.DEFAULT_SERIALIZATION_FORMAT, Locale.getDefault());
     sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
     objectMapper.setDateFormat(sdf);
-    return objectMapper;
   }
 }
